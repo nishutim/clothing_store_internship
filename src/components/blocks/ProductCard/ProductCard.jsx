fdb9106452598665ef98f1626b17aa195da93511
nishutim <nishutim@gmail.com>
2019-08-22 08:53:33 +0300

    Cleaned the code

--- src/components/blocks/ProductCard/ProductCard.jsx
+++ src/components/blocks/ProductCard/ProductCard.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import './ProductCard.css'
 
-export default function ProductCard ({ _id, title, price, images, handleProductClick }) {
-  const mainImage = images[0]
+export default function ProductCard ({ _id, title, price, images, onProductClick }) {
   return (
-    <div className="products-productcard" onClick={() => handleProductClick(_id.$oid)}>
+    <div className="products-productcard" onClick={() => onProductClick(_id.$oid)}>
       <img
-        className="products-productcard-img" src={mainImage}
+        className="products-productcard-img" src={images[0]}
         alt="title" />
       <div className="products-productcard-desc">
         <p className="products-productcard-desc__title">{title}</p>
--- src/components/blocks/Products/Products.jsx
+++ src/components/blocks/Products/Products.jsx
@@ -2,14 +2,12 @@ import React from 'react'
 import ProductCard from '../ProductCard/ProductCard'
 import './Products.css'
 
-export default function Products (
-  {
-    products,
-    filterText,
-    currentFilterTags,
-    handleProductClick,
-  }
-) {
+export default function Products ({
+  products,
+  filterText,
+  currentFilterTags,
+  onProductClick,
+}) {
   const items = []
   const {
     currentCategorieTag,
@@ -21,24 +19,44 @@ export default function Products (
   let sortedProducts = null
   if (currentSortTag === 'Our Picks') sortedProducts = [...products]
   if (currentSortTag === 'Price: low to high') {
-    sortedProducts =
-  [...products].sort((a, b) => a.price - b.price)
+    sortedProducts = [...products].sort((a, b) => a.price - b.price)
   }
-  if (currentSortTag === 'Price: high to low') sortedProducts = [...products].sort((a, b) => b.price - a.price)
+  if (currentSortTag === 'Price: high to low') {
+    sortedProducts = [...products].sort((a, b) => b.price - a.price)
+  }
 
   sortedProducts.forEach(product => {
-    if (product.tags.indexOf(currentCategorieTag) === -1 && currentCategorieTag !== 'All') return
-    if (product.size.indexOf(currentSizeTag) === -1 && currentSizeTag !== 'All') return
-    if (product.color.indexOf(currentColorTag.toLowerCase()) === -1 && currentColorTag !== 'All') return
-    if (product.title.toLowerCase().indexOf(filterText.toLowerCase().trim()) === -1) return
-    items.push(<ProductCard
-      key={product._id.$oid} handleProductClick={handleProductClick}
-      {...product} />)
+    if (
+      product.tags.indexOf(currentCategorieTag) === -1 &&
+      currentCategorieTag !== 'All'
+    ) {
+      return
+    }
+    if (
+      product.size.indexOf(currentSizeTag) === -1 &&
+      currentSizeTag !== 'All'
+    ) {
+      return
+    }
+    if (
+      product.color.indexOf(currentColorTag.toLowerCase()) === -1 &&
+      currentColorTag !== 'All'
+    ) {
+      return
+    }
+    if (
+      product.title.toLowerCase().indexOf(filterText.toLowerCase().trim()) ===
+      -1
+    ) {
+      return
+    }
+    items.push(
+      <ProductCard
+        key={product._id.$oid}
+        onProductClick={onProductClick}
+        {...product} />
+    )
   })
 
-  return (
-    <div className="products">
-      {items}
-    </div>
-  )
+  return <div className="products">{items}</div>
 }
